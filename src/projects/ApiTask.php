c3a8ad9f37c975b71651c5d5430efd51354f58c6
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2025-02-15 16:13:36 -0300

    api create

--- src/projects/ApiTask.php
+++ src/projects/ApiTask.php
@@ -16,10 +16,9 @@ if($uri == '/tasks'){
     $mapper = new TaskMapper($mysqli);
 switch($method){
     case 'GET':
-        // file_get_contents('php://input')
-        $id = $_GET['id'];
-        $task = $mapper->get($id);
-        echo json_encode($task);
+        
+       
+        echo json_encode( $mapper->getAll(10));
 break;
 case 'POST':
     $body = json_decode(file_get_contents('php://input'), true);
@@ -31,8 +30,11 @@ case 'DELETE':
     $mapper -> delete(id: $id);
     http_response_code(response_code:204);
     break;
-    
-}
+    case 'PUT':
+        $body = json_decode(file_get_contents('php://input'), true);
+        $mapper -> update(    $task = new Task($body['name'], $body['description'], new DateTime($body['scheduledAt'])));
+        http_response_code(response_code:203);
+    }
 
 
 
