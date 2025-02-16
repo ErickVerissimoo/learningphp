43b8eb44949184c13aafdd06b29be344805739d6
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2025-02-16 09:51:04 -0300

    testes

---
+++ src/projects/login/login.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Login</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <form
+        action="process.php" method="post"
+        >
+    <input type="email" name="email"><br>
+    <input 
+    <button type="submit">submeter</button>
+    </form>
+    </body>
+    <script src="scripts.js" defer></script>
+</html>
